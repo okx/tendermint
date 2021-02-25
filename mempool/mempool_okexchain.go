e0b222c5eb400df4f8a67530afcd564ed45899d7
meng <805442788@qq.com>
2021-02-25 17:42:49 +0800

    optimize GetMaxTxNumPerBlock

--- mempool/mempool_okexchain.go
+++ mempool/mempool_okexchain.go
@@ -2,17 +2,34 @@ package mempool
 
 import (
 	"strconv"
+	"sync"
 )
 
 var (
-	MaxTxNumPerBlock = "300"
+	MaxTxNumPerBlockStr       = "300"
+	MaxTxNum            int64 = 300
+	once                sync.Once
 )
+
+func string2number(input string, defaultRes int64) int64 {
+	if len(input) == 0 {
+		return defaultRes
+	}
+
+	res, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func init() {
+	once.Do(func() {
+		MaxTxNum = string2number(MaxTxNumPerBlockStr, 300)
+	})
+}
 
 //	query the MaxTxNumPerBlock from app
 func (mem *CListMempool) GetMaxTxNumPerBlock() int64 {
-	startBlockHeight, err := strconv.ParseInt(MaxTxNumPerBlock, 10, 64)
-	if err != nil {
-		return 300
-	}
-	return startBlockHeight
+	return MaxTxNum
 }
