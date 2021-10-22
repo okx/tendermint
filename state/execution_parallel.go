2c6f45a79237e344a79d8ad56ad8c0cd05269d4b
chunfengSun <516108736@qq.com>
2021-10-21 22:53:28 -0500

    add paralleled-tx (#93)
    * add command
    
    * Update execution_parallel.go

--- state/execution_parallel.go
+++ state/execution_parallel.go
@@ -9,6 +9,10 @@ import (
 	dbm "github.com/tendermint/tm-db"
 )
 
+var (
+	FlagParalleledTx = "paralleled-tx"
+)
+
 func execBlockOnProxyAppAsync(
 	logger log.Logger,
 	proxyAppConn proxy.AppConnConsensus,
@@ -71,6 +75,11 @@ func execBlockOnProxyAppAsync(
 		}
 	}
 
+	// avoid panic when handle callback
+	proxyCb := func(req *abci.Request, res *abci.Response) {
+		return
+	}
+	proxyAppConn.SetResponseCallback(proxyCb)
 	proxyAppConn.PrepareParallelTxs(AsyncCb, transTxsToBytes(block.Txs))
 
 	// Run txs of block.
